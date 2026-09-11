4d6de018a2858d6fe2bd25c20ac74aea2ff4dd52
vpdharshini30 <146475389+vpdharshini30@users.noreply.github.com>
2026-09-11 15:15:29 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,127 @@
+package com.libraymanagement; 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		Scanner sc=new Scanner(System.in);
+     
+		ArrayList<BOOK> books = new ArrayList<>();
+	
+		int choice,bookid,stdid;
+		// LIBRARY Objects
+        LIBRARY library = new LIBRARY();
+        //LIBRARIAN Objects
+        LIBRARIAN librarian = new LIBRARIAN();
+        librarian.addOldIssuedStudents(library);
+        //Students Object
+       
+        do  {
+
+            System.out.println("\n=========================");
+            System.out.println(" LIBRARY MANAGEMENT SYSTEM");
+            System.out.println(" =========================");
+            System.out.println("1. Add Book");  
+            System.out.println("2. View Books");
+            System.out.println("3. Search Book");
+            System.out.println("4. Book Availability");
+            System.out.println("5. Approve Book Issue");
+            System.out.println("6. Return Book / Collect Fine");
+            System.out.println("7. Due Date Reminder");
+            System.out.println("8. Generate Report");
+            System.out.println("9. Exit");
+            System.out.println(" =========================");
+            
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+            switch(choice) {
+            case 1://Add Books
+            	System.out.print("Enter The BOOK ID : ");
+            	bookid=sc.nextInt();
+            	sc.nextLine();
+            	System.out.print("Enter The BOOK Name :");
+            	String bookname = sc.nextLine();
+            	
+                System.out.print("Enter The Author Name :");
+                String author = sc.nextLine();
+               
+                System.out.print("Enter The Category : ");
+                String category = sc.nextLine();
+                
+                BOOK book = new BOOK(bookid,bookname,author,category,true);
+                library.addBook(book);
+                break;
+                
+            case 2://View BOOk
+            	library.viewBooks();
+            	break;
+            	
+            case 3://Search book
+            	System.out.print("Please Enter The BookId to Search The Book : ");
+            	bookid=sc.nextInt();
+            	BOOK searchBook=library.searchBook(bookid);
+            	 System.out.println("\n-----------------------------------------------------------------------------------------------");
+      		   System.out.printf("%-10s %-25s %-25s %-20s %-15s%n","BOOK ID", "BOOK NAME","AUTHOR","CATEGORY","AVAILABILITY");
+      			System.out.println("-----------------------------------------------------------------------------------------------");
+      		  
+            	if(searchBook!=null) {
+            		searchBook.displayBookDetails();
+            		
+            	}
+            	else {
+            		System.err.println("Book Not Found");
+            	}
+            	break;
+            	
+            case 4://Available Book
+            	
+            	library.availableBooks();
+            	break;
+            	
+            case 5://Approve Book Issue
+            	System.out.print("Enter the BookId : ");
+            	bookid=sc.nextInt();
+            	System.out.print("Enter The Student Id : ");
+            	stdid=sc.nextInt();
+            	STUDENT student5 = library.searchStudent(stdid);
+            	librarian.approveBookIssue(library, student5, bookid);
+            	break;
+            	
+            	
+            case 6://Return Books
+            	System.out.print("Enter The Student ID : ");
+            	stdid=sc.nextInt();
+                STUDENT student = library.searchStudent(stdid);
+                if (student != null) {
+                    librarian.returnBook(student);
+                } else 
+                {
+                    System.out.println("Student Not Found!");
+                }
+                break;
+            case 7://DUE date 
+            	
+               System.out.print("Enter The Student Id : ");
+               stdid=sc.nextInt();
+               LIBRARIAN.dueDate(library,stdid);
+               break;
+            
+            case 8://Report 
+            	library.generateReport();
+            	break;
+            case 9://Exit
+            	System.out.println("Thank You.............");
+            	break;
+            default:
+            	System.err.println("Try Again Later");
+            }
+            
+        }while(choice!=9);
+	}
+
+	}
+
+
